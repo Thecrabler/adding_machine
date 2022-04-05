c158fb1bc05d3b58b24f2adb6f42859f982240a1
Thecrabler <102967159+Thecrabler@users.noreply.github.com>
2022-04-05 07:59:15 -0400

    Update addingodot.py

--- addingodot.py
+++ addingodot.py
@@ -1,10 +1,4 @@
-while True:
-    print('\n','this is adding machine','\n')
-    try:
-        first_number = int(input("what is the first number you want to add >"))
-        second_number = int(input('what is the second number you want to add >'))
-        sum=first_number+second_number
-        print('the of the numbers is',sum)
-        break
-    except:
-        print('Your input is not correct')
+first_number = int(input("what is the first number you want to add >"))
+second_number = int(input('what is the second number you want to add >'))
+sum=first_number+second_number
+print('the of the numbers is',sum)
